549ac09465b34072a3c7de719503cd31ae8a55f0
Roushith <91108113+Roushith@users.noreply.github.com>
2023-02-13 14:48:43 +0530

    Create date-and-time.java

---
+++ https:/www.hackerrank.com/challenges/date-and-time.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+
+    }
+
+}
+
+public class Solution {
